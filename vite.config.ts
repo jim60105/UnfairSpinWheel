7c6579fb27b85f2d7a23c5c04108184b2c696772
陳鈞 <jim60105@gmail.com>
2023-10-16 21:29:57 +0800

    refactor(dependencies): Remove unused packages and update build target

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    target: 'esnext' //browsers can handle the latest ES features, including top-level await
+    target: 'es2022'
   },
-  plugins: [vue(), vueJsx()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
