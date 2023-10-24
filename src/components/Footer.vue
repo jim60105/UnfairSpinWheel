04fe7fcfa9d470238a35398739e59c9ee3db45dd
陳鈞 <jim60105@gmail.com>
2023-10-24 23:18:12 +0800

    feat(CI): Add version number during CI build time

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,27 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <footer class="fixed bottom-0 left-0 m-2 text-xs z-2 overflow-hidden w-full">
-    <a href="https://github.com/jim60105/UnfairSpinWheel" target="_blank"
-      >v.{{ version }}<i class="pi pi-github vertical-align-middle m-2"></i>AGPL-3.0 Licensed</a
+    <a :href="url" target="_blank"
+      >{{ version }}<i class="pi pi-github vertical-align-middle m-2"></i>AGPL-3.0 Licensed</a
     >
     / Copyright © 2023 jim60105
   </footer>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
-const version = ref('DEVELOP');
+const hash: string | undefined = import.meta.env.VITE_GIT_HASH;
+const baseUrl = 'https://github.com/jim60105/UnfairSpinWheel';
+const version = ref('v.DEVELOP');
+const url = ref(baseUrl);
+
+onMounted(() => {
+  if (hash) {
+    version.value = 'v.' + hash.substring(0, 7).toUpperCase();
+    url.value = `${baseUrl}/tree/${hash}`;
+  }
+});
 </script>
 
 <style scoped>
