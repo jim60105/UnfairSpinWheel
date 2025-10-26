509fa0277a70266b35a2da43cab1f3e424506c51
leafwind <leafwind.cs@gmail.com>
2025-10-27 00:03:21 +0900

    fix

--- src/interface/IItem.ts
+++ src/interface/IItem.ts
@@ -1,6 +1,8 @@
+import { type AudioSetting } from '@/services/SettingService';
 export interface IItem {
   group: string;
   label: string;
   weight: number;
   order: number;
+  congratulationSound?: AudioSetting;
 }
